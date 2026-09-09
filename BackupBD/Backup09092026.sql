9240502b32ce1752ab6a8b977e39d1a34d66a050
Negrxz <anabelpacheco309@gmail.com>
2026-09-09 20:48:50 -0300

    coreccion del campo activo

--- BackupBD/Backup09092026.sql
+++ BackupBD/Backup09092026.sql
@@ -26,13 +26,14 @@ CREATE TABLE IF NOT EXISTS `cartilla` (
   `fecha_inicio` date NOT NULL,
   `fecha_fin` date NOT NULL,
   `id_marca` int NOT NULL DEFAULT (0),
+  `activo` tinyint(1) NOT NULL,
   PRIMARY KEY (`id_cartilla`),
   UNIQUE KEY `numero_cartilla` (`numero_cartilla`),
   KEY `FK_cartilla_marca` (`id_marca`),
   CONSTRAINT `FK_cartilla_marca` FOREIGN KEY (`id_marca`) REFERENCES `marca` (`id_marca`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla base_de_datos_cartilla.cartilla: ~0 rows (aproximadamente)
 
 -- Volcando estructura para tabla base_de_datos_cartilla.cliente
 CREATE TABLE IF NOT EXISTS `cliente` (
@@ -42,11 +43,12 @@ CREATE TABLE IF NOT EXISTS `cliente` (
   `dni` varchar(15) NOT NULL DEFAULT '',
   `telefono` varchar(20) DEFAULT '0',
   `direccion` varchar(100) DEFAULT NULL,
+  `activo` tinyint(1) NOT NULL,
   PRIMARY KEY (`id_cliente`),
   UNIQUE KEY `dni` (`dni`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla base_de_datos_cartilla.cliente: ~0 rows (aproximadamente)
 
 -- Volcando estructura para tabla base_de_datos_cartilla.detalle_pedido
 CREATE TABLE IF NOT EXISTS `detalle_pedido` (
@@ -56,6 +58,7 @@ CREATE TABLE IF NOT EXISTS `detalle_pedido` (
   `cantidad` int NOT NULL,
   `precio_unitario` decimal(10,2) NOT NULL DEFAULT (0),
   `subtotal` decimal(10,2) NOT NULL DEFAULT (0),
+  `activo` tinyint(1) NOT NULL,
   PRIMARY KEY (`id_detalle`),
   KEY `FK_detalle_pedido_pedido` (`id_pedido`),
   KEY `FK_detalle_pedido_producto` (`id_producto`),
@@ -63,17 +66,18 @@ CREATE TABLE IF NOT EXISTS `detalle_pedido` (
   CONSTRAINT `FK_detalle_pedido_producto` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id_producto`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla base_de_datos_cartilla.detalle_pedido: ~0 rows (aproximadamente)
 
 -- Volcando estructura para tabla base_de_datos_cartilla.marca
 CREATE TABLE IF NOT EXISTS `marca` (
   `id_marca` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) NOT NULL,
+  `activo` tinyint(1) NOT NULL,
   PRIMARY KEY (`id_marca`),
   UNIQUE KEY `nombre` (`nombre`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla base_de_datos_cartilla.marca: ~0 rows (aproximadamente)
 
 -- Volcando estructura para tabla base_de_datos_cartilla.pago
 CREATE TABLE IF NOT EXISTS `pago` (
@@ -82,12 +86,13 @@ CREATE TABLE IF NOT EXISTS `pago` (
   `importe` decimal(10,2) NOT NULL DEFAULT (0),
   `medio_pago` varchar(30) NOT NULL DEFAULT '0',
   `id_pedido` int NOT NULL DEFAULT (0),
+  `activo` tinyint(1) NOT NULL,
   PRIMARY KEY (`id_pago`),
   KEY `FK_Pago_pedido` (`id_pedido`),
   CONSTRAINT `FK_Pago_pedido` FOREIGN KEY (`id_pedido`) REFERENCES `pedido` (`id_pedido`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla base_de_datos_cartilla.pago: ~0 rows (aproximadamente)
 
 -- Volcando estructura para tabla base_de_datos_cartilla.pedido
 CREATE TABLE IF NOT EXISTS `pedido` (
@@ -95,12 +100,13 @@ CREATE TABLE IF NOT EXISTS `pedido` (
   `fecha` date NOT NULL,
   `total` decimal(10,2) NOT NULL DEFAULT (0),
   `id_cliente` int NOT NULL DEFAULT (0),
+  `activo` tinyint(1) NOT NULL,
   PRIMARY KEY (`id_pedido`),
   KEY `FK_Pedido_cliente` (`id_cliente`),
   CONSTRAINT `FK_Pedido_cliente` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id_cliente`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla base_de_datos_cartilla.pedido: ~0 rows (aproximadamente)
 
 -- Volcando estructura para tabla base_de_datos_cartilla.producto
 CREATE TABLE IF NOT EXISTS `producto` (
@@ -110,12 +116,13 @@ CREATE TABLE IF NOT EXISTS `producto` (
   `precio` decimal(10,2) NOT NULL DEFAULT (0),
   `stock` int NOT NULL DEFAULT (0),
   `id_cartilla` int NOT NULL,
+  `activo` tinyint(1) NOT NULL,
   PRIMARY KEY (`id_producto`),
   KEY `FK_Producto_cartilla` (`id_cartilla`),
   CONSTRAINT `FK_Producto_cartilla` FOREIGN KEY (`id_cartilla`) REFERENCES `cartilla` (`id_cartilla`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla base_de_datos_cartilla.producto: ~0 rows (aproximadamente)
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
